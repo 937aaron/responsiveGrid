4a939d423c50a557a7c8b06cf0299a01c753734e
Aaron <newcomer.aaron@icloud.com>
2016-06-01 17:07:22 -0400

    initial commit

---
+++ assets/grid.js
@@ -0,0 +1,140 @@
+x= 1+1;
+console.log("hello");
+
+$(".c").mouseover(function(){
+  $(".c").text("C");
+  $(".c").mouseleave(function(){
+  $(".c").text("");
+  })
+})
+$(".c").click(function(){
+  $("#Cnote").get(0).play();
+  console.log("Hello")
+});
+$(".d").mouseover(function(){
+  $(".d").text("D");
+  $(".d").mouseleave(function(){
+  $(".d").text("");
+  })
+})
+
+$(".d").click(function(){
+  $("#Dnote").get(0).play();
+  console.log("Hello")
+});
+
+$(".e").mouseover(function(){
+  $(".e").text("E");
+  $(".e").mouseleave(function(){
+  $(".e").text("");
+  })
+})
+
+$(".e").click(function(){
+  $("#Enote").get(0).play();
+  console.log("Hello")
+});
+
+$(".f").mouseover(function(){
+  $(".f").text("F");
+  $(".f").mouseleave(function(){
+  $(".f").text("");
+  })
+})
+
+$(".f").click(function(){
+  $("#Fnote").get(0).play();
+  console.log("Hello")
+});
+
+$(".g").mouseover(function(){
+  $(".g").text("G");
+  $(".g").mouseleave(function(){
+  $(".g").text("");
+  })
+})
+
+$(".g").click(function(){
+  $("#Gnote").get(0).play();
+  console.log("Hello")
+});
+
+$(".a").mouseover(function(){
+  $(".a").text("A");
+  $(".a").mouseleave(function(){
+  $(".a").text("");
+  })
+})
+
+$(".a").click(function(){
+  $("#Anote").get(0).play();
+  console.log("Hello")
+});
+
+$(".b").mouseover(function(){
+  $(".b").text("B");
+  $(".b").mouseleave(function(){
+  $(".b").text("");
+  })
+})
+
+$(".b").click(function(){
+  $("#Bnote").get(0).play();
+  console.log("Hello")
+});
+
+
+$(".dflat").mouseover(function(){
+  $(".dflat").text("Db");
+  $(".dflat").mouseleave(function(){
+  $(".dflat").text("");
+  })
+})
+$(".dflat").click(function(){
+  $("#Dflatnote").get(0).play();
+  console.log("Hello")
+});
+
+$(".eflat").mouseover(function(){
+  $(".eflat").text("Eb");
+  $(".eflat").mouseleave(function(){
+  $(".eflat").text("");
+  })
+})
+$(".eflat").click(function(){
+  $("#Eflatnote").get(0).play();
+  console.log("Hello")
+});
+
+$(".gflat").mouseover(function(){
+  $(".gflat").text("Gb");
+  $(".gflat").mouseleave(function(){
+  $(".gflat").text("");
+  })
+})
+$(".gflat").click(function(){
+  $("#Gflatnote").get(0).play();
+  console.log("Hello")
+});
+
+$(".aflat").mouseover(function(){
+  $(".aflat").text("Ab");
+  $(".aflat").mouseleave(function(){
+  $(".aflat").text("");
+  })
+})
+$(".aflat").click(function(){
+  $("#Aflatnote").get(0).play();
+  console.log("Hello")
+});
+
+$(".bflat").mouseover(function(){
+  $(".bflat").text("Bb");
+  $(".bflat").mouseleave(function(){
+  $(".bflat").text("");
+  })
+})
+$(".bflat").click(function(){
+  $("#Bflatnote").get(0).play();
+  console.log("Hello")
+});
